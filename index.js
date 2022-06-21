2d83d98658eda9a05f61a743e16619330a031dca
Fernanda Ferreira <fefcufe@gmail.com>
2022-06-21 18:38:41 -0300

    feat: adiciona requisito 3

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const readFIle = require('./helpers/readFile');
+const randomToken = require('./helpers/generateToken');
+
+/* const loginMiddleware = require('./middlewares/loginMiddleware'); */
 
 const app = express();
 app.use(bodyParser.json());
@@ -38,5 +41,21 @@ app.get('/talker/:id', async (req, res) => {
     res.status(200).json(talker);
   } catch (error) {
     res.status(400).send('Erro!'); 
+  }
+});
+
+// requisito 3
+app.post('/login', (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if ([email, password].includes(undefined)) {
+      return res.status(400).json({ message: 'Faltando informações.' });
+    }
+  
+  const token = randomToken();
+  console.log(token);
+  return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).end();
   }
 });
