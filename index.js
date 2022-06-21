8e8addf53705a594bece4560fcb3668976998296
Fernanda Ferreira <fefcufe@gmail.com>
2022-06-21 16:35:47 -0300

    feat: Adiciona requisito 2

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
+// requisito 1
 app.get('/talker', async (req, res) => {
   try {
     const talkersList = await readFIle();
@@ -24,5 +25,18 @@ app.get('/talker', async (req, res) => {
     res.status(200).json(talkersList);
   } catch (error) {
     res.status(400).json({ message: error });
+  }
+});
+
+// requisito 2
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const talkers = await readFIle();
+    const { id } = req.params;
+    const talker = talkers.find((eachTalker) => eachTalker.id === Number(id));
+    if (!talker) res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    res.status(200).json(talker);
+  } catch (error) {
+    res.status(400).send('Erro!'); 
   }
 });
